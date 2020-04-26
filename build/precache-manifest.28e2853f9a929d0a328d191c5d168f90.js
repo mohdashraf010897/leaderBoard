cb03a993a9176f951083cefce6cfaab4e33e0d6e
mohdashraf010897 <mohdashraf010897@gmail.com>
2020-04-26 18:01:07 +0530

    Changed TAB ICON and added optimised build

---
+++ build/precache-manifest.28e2853f9a929d0a328d191c5d168f90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5411cf980d4e34754df3c78e0a47ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "015b6afcb0089f314894",
+    "url": "/static/css/2.f1d36a8f.chunk.css"
+  },
+  {
+    "revision": "6df4eebb8c07f39ae056",
+    "url": "/static/css/main.7ca74f7b.chunk.css"
+  },
+  {
+    "revision": "015b6afcb0089f314894",
+    "url": "/static/js/2.542a5741.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.542a5741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df4eebb8c07f39ae056",
+    "url": "/static/js/main.6cb8b74e.chunk.js"
+  },
+  {
+    "revision": "801788f0384e2a6982fb",
+    "url": "/static/js/runtime-main.14d28943.js"
+  },
+  {
+    "revision": "9211b907a4345089fa1bd8d702cd0f4c",
+    "url": "/static/media/logo.9211b907.svg"
+  }
+]);
